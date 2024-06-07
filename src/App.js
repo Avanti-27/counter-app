acbd1e4c6affe2998a5687eb502125f0ce6d1a8f
Avanti <avanti.tatwawadi@gmail.com>
2024-06-07 23:50:30 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleAdd = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleSubtract = () => {
+    setCounter(counter - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center">
+      <h1 className="text-4xl font-bold p-4 m-8">Counter App</h1>
+      <div className="bg-black text-white text-3xl mx-auto my-0 w-2/4">
+        <div className="p-4 m-4">{counter}</div>
+        <div>
+          <button
+            className="border-2 border-white px-4 py-1 m-4 rounded-lg"
+            onClick={handleAdd}
+          >
+            +
+          </button>
+          <button
+            className="border-2 border-white px-4 py-1 m-4 rounded-lg"
+            onClick={handleSubtract}
+          >
+            -
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
